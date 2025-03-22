7fdabe620547bea211f19235246b7cbab76d606a
East_Arctica <eastarctica@gmail.com>
2025-03-21 22:47:18 -0400

    Remove eslint

--- .erb/configs/webpack.config.main.dev.ts
+++ .erb/configs/webpack.config.main.dev.ts
@@ -6,15 +6,8 @@ import path from 'path';
 import webpack from 'webpack';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { merge } from 'webpack-merge';
-import checkNodeEnv from '../scripts/check-node-env';
 import baseConfig from './webpack.config.base';
 import webpackPaths from './webpack.paths';
-
-// When an ESLint server is running, we can't set the NODE_ENV so we'll check if it's
-// at the dev webpack config is not accidentally run in a production environment
-if (process.env.NODE_ENV === 'production') {
-  checkNodeEnv('development');
-}
 
 const configuration: webpack.Configuration = {
   devtool: 'inline-source-map',
@@ -37,7 +30,6 @@ const configuration: webpack.Configuration = {
   },
 
   plugins: [
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     new BundleAnalyzerPlugin({
       analyzerMode: process.env.ANALYZE === 'true' ? 'server' : 'disabled',
--- .erb/configs/webpack.config.renderer.dev.ts
+++ .erb/configs/webpack.config.renderer.dev.ts
@@ -9,19 +9,11 @@ import { execSync, spawn } from 'child_process';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import baseConfig from './webpack.config.base';
 import webpackPaths from './webpack.paths';
-import checkNodeEnv from '../scripts/check-node-env';
-
-// When an ESLint server is running, we can't set the NODE_ENV so we'll check if it's
-// at the dev webpack config is not accidentally run in a production environment
-if (process.env.NODE_ENV === 'production') {
-  checkNodeEnv('development');
-}
 
 const port = process.env.PORT || 1212;
 const manifest = path.resolve(webpackPaths.dllPath, 'renderer.json');
 const skipDLLs =
-  module.parent?.filename.includes('webpack.config.renderer.dev.dll') ||
-  module.parent?.filename.includes('webpack.config.eslint');
+  module.parent?.filename.includes('webpack.config.renderer.dev.dll');
 
 /**
  * Warn if the DLL is not built
